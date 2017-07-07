1f0e30e678de8e5e730b51e7a9dd81229ddc7dfb
Michal <Michal.stachnik@my.liu.edu>
2017-07-07 19:51:36 -0400

    some modifications

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ var clock = document.getElementById('clock')
 
 clock.innerHTML = mins
 
+var myTimeout = setInterval(function(){
+                console.log('firing')
+            }, 1000)
 function startTimer(){
 
     var currentDisplay = Number(clock.innerHTML)
     toggleStart = !toggleStart
     console.log(toggleStart)
+    
     if(toggleStart){
-        setInterval(function(){
-            currentDisplay--
-            console.log(currentDisplay)
-            clock.innerHTML = currentDisplay
-        }, 1000)
+        
+    }
+    else if(!toggleStart){
+        
+        clearInterval(myTimeout)
     }
 }
 clock.onclick = function(){

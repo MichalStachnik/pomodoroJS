2eba74e0b83e33d3960877787da09f5f7964bddd
Michal <Michal.stachnik@my.liu.edu>
2017-07-08 16:20:24 -0400

    countdown and buttons working

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ var toggleStart = false
 var mins = 25
 var secs = 0 
 var clock = document.getElementById('clock')
+var controlsSpan = document.getElementById('controlsSpan')
+var plus = document.getElementById('plus')
+var minus = document.getElementById('minus')
 
 clock.innerHTML = mins
+controlsSpan.innerHTML = clock.innerHTML
+
+plus.onclick = function(){
+    toggleStart = false
+    controlsSpan.innerHTML = Number(controlsSpan.innerHTML) + 1
+    clock.innerHTML = controlsSpan.innerHTML
+}
+minus.onclick = function(){
+    toggleStart = false
+    controlsSpan.innerHTML = Number(controlsSpan.innerHTML) - 1
+    clock.innerHTML = controlsSpan.innerHTML
+}
 
 clock.onclick = function(){
     console.log('clicked')
@@ -15,10 +30,9 @@ clock.onclick = function(){
         }
         else{
             var currentDisplay = Number(clock.innerHTML)
-            console.log('firing')
-            console.log(toggleStart)
+            currentDisplay--
+            clock.innerHTML = currentDisplay
+            console.log(currentDisplay)
         }
-    
     }
-      
 }

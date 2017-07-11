9e9744fb994edeee32a900bcea470d5d9f8d2669
Michal <Michal.stachnik@my.liu.edu>
2017-07-11 16:50:59 -0400

    pretty much fully functional

--- app.js
+++ app.js
@@ -1,38 +1,65 @@
 var toggleStart = false
 var mins = 25
-var secs = 0 
+var secs = 0
 var clock = document.getElementById('clock')
 var controlsSpan = document.getElementById('controlsSpan')
 var plus = document.getElementById('plus')
 var minus = document.getElementById('minus')
+var startStop = document.getElementById('startStop')
+var reset = document.getElementById('reset')
 
-clock.innerHTML = mins
+clock.innerHTML = mins + ':0' + secs
 controlsSpan.innerHTML = clock.innerHTML
 
+formatTime = function(mins, secs){
+    if(secs < 10){
+        return mins.toString() + ':' + '0' + secs.toString()
+    }
+    else{
+        return mins.toString() + ':' + secs.toString()
+    }
+}
 plus.onclick = function(){
     toggleStart = false
-    controlsSpan.innerHTML = Number(controlsSpan.innerHTML) + 1
-    clock.innerHTML = controlsSpan.innerHTML
+    mins++
+    var timeToDisplay = formatTime(mins, secs)
+    controlsSpan.innerHTML = timeToDisplay
+    clock.innerHTML = timeToDisplay
 }
 minus.onclick = function(){
     toggleStart = false
-    controlsSpan.innerHTML = Number(controlsSpan.innerHTML) - 1
+    mins--
+    var timeToDisplay = formatTime(mins, secs)
+    controlsSpan.innerHTML = timeToDisplay
+    clock.innerHTML = timeToDisplay
+}
+reset.onclick = function(){
+    console.log('clicked')
+    toggleStart = false
     clock.innerHTML = controlsSpan.innerHTML
 }
-
-clock.onclick = function(){
-    console.log('clicked')
+startStop.onclick = function(){
     var myTimeout = setInterval(timer, 1000)
     toggleStart = !toggleStart
+    if(toggleStart){
+        startStop.innerHTML = 'STOP'
+    }
+    else{
+        startStop.innerHTML = 'START'
+    }
     function timer(){
         if(!toggleStart){
             clearInterval(myTimeout)
         }
         else{
-            var currentDisplay = Number(clock.innerHTML)
-            currentDisplay--
-            clock.innerHTML = currentDisplay
-            console.log(currentDisplay)
+            if(secs === 0){
+                mins--
+                secs = 60
+            }
+            secs--
+            var timeToDisplay = formatTime(mins, secs)
+            console.log(timeToDisplay)
+            clock.innerHTML = timeToDisplay
         }
     }
 }
